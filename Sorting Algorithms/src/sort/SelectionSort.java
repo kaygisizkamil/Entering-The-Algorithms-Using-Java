55d1493ad5035e1e55847da5567d4854bf5149e2
kaygisizkamil <kamilkaygisizz@gmail.com>
2022-09-25 00:43:42 +0300

    some explanation has added for better understanding unstable selection sort

--- Sorting Algorithms/src/sort/SelectionSort.java
+++ Sorting Algorithms/src/sort/SelectionSort.java
@@ -2,15 +2,17 @@ package sort;
 
 import additional.Swap;
 
-public class SelectionSort {
+public class SelectionSort {//unstable
 	
 	public final static void sort(int []arr) {
 		
-		int min;
+		int min,j;
 		System.out.println("Here comes selection sort");
-		for(int i=0;i<arr.length;i++) {
+		for(int i=0;i<arr.length-1;i++) {
 			 min=i;
-			for(int j=i;j<arr.length-1;j++) {
+			for(j=i;j<arr.length;j++) {
+				//get the i.th element compare that element through the last element and dont swap till
+				//find the smaller element than our elementarr[min] than do the swap
 				 if(arr[j]<arr[min]) min=j;
 			}
 			Swap.swap(arr,min,i);
