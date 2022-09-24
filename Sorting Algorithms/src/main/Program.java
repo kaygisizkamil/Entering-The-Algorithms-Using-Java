38cc073be54cb950648e20317bd2281419948ef3
kaygisizkamil <kamilkaygisizz@gmail.com>
2022-09-25 00:44:07 +0300

    calling insertion sort inside

--- Sorting Algorithms/src/main/Program.java
+++ Sorting Algorithms/src/main/Program.java
@@ -1,10 +1,15 @@
 package main;
 
+import additional.PrintArray;
+
 public class Program {
+	
 
 	public static void main(String[] args) {
+	
 		// TODO Auto-generated method stub
-		int[] arr=additional.GenerateArray.generateArr();		
+		int[] arr=additional.GenerateArray.generateArr();	
+		
 		additional.PrintArray.print(arr,false);
 		sort.BubbleSort.sort(arr);	
 		additional.PrintArray.print(arr,true);
@@ -14,6 +19,13 @@ public class Program {
 		additional.PrintArray.print(arr,false);
 		sort.SelectionSort.sort(arr);	
 		additional.PrintArray.print(arr,true);
+		
+
+		arr=additional.GenerateArray.generateArr();	
+		additional.PrintArray.print(arr,false);
+		sort.InsertionSort.sort(arr);	
+		additional.PrintArray.print(arr,true);
+		
 		
 		
 
